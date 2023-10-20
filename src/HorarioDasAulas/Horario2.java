753d44e4eeb34b81b96a3dd14f39b238cbf35382
Thales Eduardo <89024257+thales32k0@users.noreply.github.com>
2023-10-20 10:05:37 -0300

    Update Horario2.java

--- src/HorarioDasAulas/Horario2.java
+++ src/HorarioDasAulas/Horario2.java
@@ -1,2 +1,26 @@
-package HorarioDasAulas;public class Horario2 {
+package HorarioDasAulas;
+
+    public class Horario2 {
+
+      public static void main(String[] args) {
+        //Terça
+        ArrayList<String> HorariosDeJava = new ArrayList<String>();
+        HorariosDeJavas.add("Sintaxe Básica das 07:00 as 08:00");
+        HorariosDeJava.add("Orientação a Objetos 08:00 as 09:00");
+        HorariosDeJava.add("Coleção em Java 09:00 as 10:00");
+        HorariosDeJava.add("Tratamento e Exceções 10:00 as 11:00");
+        HorariosDeJava.add("Threads e Multithreading 11:00 as 12:00");
+
+        //Quarta
+        HorariosDeJavas.add(Desenvolvimento Web em Java 07:00 as 08:00);
+        HorariosDeJavas.add("Entrada de Saida I/O 08:00 as 09:00");
+        HorariosDeJavas.add("Bibliotecas Padrão 09:00 as 10:00");
+        HorariosDeJavas.add(Java Database Connectivity 10:00 as 11:00);
+        HorariosDeJavas.add(Interfaces Gráficas de Usuário 11:00 as 12:00);
+        
+        for (String horarios: HorariosDeJava) {
+            System.out.println(horarios);
+        
+  }
+ }      
 }
