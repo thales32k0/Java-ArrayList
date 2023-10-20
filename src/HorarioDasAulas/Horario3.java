4fea06165db40fe1bb62d9295bae18c3f099d74b
Thales Eduardo <89024257+thales32k0@users.noreply.github.com>
2023-10-20 13:24:19 -0300

    Update Horario3.java

--- src/HorarioDasAulas/Horario3.java
+++ src/HorarioDasAulas/Horario3.java
@@ -1,2 +1,22 @@
-package HorarioDasAulas;public class Horario3 {
+package HorarioDasAulas;
+  
+  public class Horario3 {
+
+    ArrayList<String> HorarioOrientaçãoObjeto = new ArrayList<String>();
+
+    //Quinta Feira
+    HorarioOrientaçãoObjeto.add("Classes e Objetos 07:00 as 08:00");
+    HorarioOrientaçãoObjeto.add("Encapsulamento 08:00 as 09:00");
+    HorarioOrientaçãoObjeto.add("Herança 09:00 as 10:00");
+    HorarioOrientaçãoObjeto.add("Polimorfismo 10:00 as 11:00");
+
+    //Sexta Feira
+    HorarioOrientaçãoObjeto.add(Abstração 07:00 as 08:00);
+    HorarioOrientaçãoObjeto.add(Metódos e prioridades 08:00 as 09:00);
+    HorarioOrientaçãoObjeto.add(Construtores 10:00 as 11:00);
+    HorarioOrientaçãoObjeto.add(Associação 11:00 as 12:00);
+       
+        for (String aulas : HorarioOrientaçãoObjeto) {
+            System.out.println(aulas);                          
+  
 }
