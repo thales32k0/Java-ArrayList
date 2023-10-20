a43bab583dfb44fd47a1443c34e08896ab5eb980
Thales Eduardo <89024257+thales32k0@users.noreply.github.com>
2023-10-20 08:03:52 -0300

    Update InteligenciaArtificial.java

--- src/Aulas/InteligenciaArtificial.java
+++ src/Aulas/InteligenciaArtificial.java
@@ -1,2 +1,17 @@
-package Aulas;public class InteligenciaArtificial {
+package Aulas;
+  public class InteligenciaArtificial {
+    public static void main(String[] args) {
+
+      ArrayList<String> listaDeAulas = new ArrayList<String>();
+      listaDeAulas.add("Fundamentos da matemática");
+      listaDeAulas.add("Ética da IA");
+      listaDeAulas.add("Aprendizado de Máquina");
+      listaDeAulas.add("Processamento de linguagem natural");
+      listaDeAulas.add("Visão Computacional");
+
+      for (String nome : listaDeAulas) {
+            System.out.println(nome);
+    
+      }
+    }
 }
