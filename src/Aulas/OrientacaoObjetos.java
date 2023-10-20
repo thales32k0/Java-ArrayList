90c6017921144f5d2ac16ed80cf8fad60d14b561
Thales Eduardo <89024257+thales32k0@users.noreply.github.com>
2023-10-20 09:05:46 -0300

    Update OrientacaoObjetos.java

--- src/Aulas/OrientacaoObjetos.java
+++ src/Aulas/OrientacaoObjetos.java
@@ -1,2 +1,21 @@
-package Aulas;public class OrientacaoObjetos {
+package Aulas;
+    
+    public class OrientacaoObjetos {
+
+      public static void main(String[] args) {
+
+        ArrayList<String> listaDeAulas = new ArrayList<String>();
+
+        listaDeAulas.add("Classes e Objetos");
+        listaDeAulas.add("Encapsulamento");
+        listaDeAulas.add("Herança");
+        listaDeAulas.add("Polimorfismo");
+        listaDeAulas.add("Abstração");
+        listaDeAulas.add("Métodos e Propriedades");
+        listaDeAulas.add("Construtores");
+        listaDeAulas.add("Associação");
+
+        for (String aulas : listaDeAulas) {
+            System.out.println(aulas);
+  }
 }
