d1cfc82831afbe434e6f2a17f91fc93898c6d1ec
Thales Eduardo <89024257+thales32k0@users.noreply.github.com>
2023-10-20 09:42:21 -0300

    Update HorarioInteligenciaArt.java

--- src/HorarioDasAulas/HorarioInteligenciaArt.java
+++ src/HorarioDasAulas/HorarioInteligenciaArt.java
@@ -6,12 +6,12 @@ package HorarioDasAulas;
 
         ArrayList<String> horarioInteligenciaArt = new ArrayList<String>();
           //Segunda 
-          lista.horarioInteligenciaArt.add("Fundamentos da Matemática das 07:00 as 08:00");
-          lista.horarioInteligenciaArt.add("Ética da IA das 09:00 as 10:00");
-          lista.horarioInteligenciaArt.add("Aprendizado de máquinas das 13:00 as 14:00");
-          lista.horarioInteligenciaArt.add("Processamento de linguagem natural das 16:00 as 17:00");
+          horarioInteligenciaArt.add("Fundamentos da Matemática das 07:00 as 08:00");
+          horarioInteligenciaArt.add("Ética da IA das 09:00 as 10:00");
+          horarioInteligenciaArt.add("Aprendizado de máquinas das 13:00 as 14:00");
+          horarioInteligenciaArt.add("Processamento de linguagem natural das 16:00 as 17:00");
           //Quarta
-          lista.horarioInteligenciaArt.add("Visão Computacional 08:00 as 09:00");
+          horarioInteligenciaArt.add("Visão Computacional 08:00 as 09:00");
              
           for (String horarios : horarioInteligenciaArt) {
             System.out.println(horarios);
