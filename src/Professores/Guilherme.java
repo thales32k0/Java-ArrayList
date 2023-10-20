2ced3bf8671aa8bcd994cae0e91d52547cccfa41
Thales Eduardo <89024257+thales32k0@users.noreply.github.com>
2023-10-20 13:40:10 -0300

    Update Guilherme.java

--- src/Professores/Guilherme.java
+++ src/Professores/Guilherme.java
@@ -1,2 +1,15 @@
-package Professores;public class Guilherme {
-}
+package Professores;
+
+  public class Guilherme {
+
+    public static void main(String[] args) {
+
+      ArrayList<String> AulasGuilherme = new ArrayList<String>();
+
+      AulasGuilherme.add("Responsável pelas aulas de Inteligencia Artificial");
+      
+        for (String aulas : AulasGuilherme) {
+            System.out.println(aulas);
+  }
+ }
+}  
